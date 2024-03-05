b886f63500f08fbd5a4e2dc699da36a2d85cc260
Gui webber <guiz1n.webber@gmail.com>
2024-03-05 14:30:44 -0300

    correcting

--- src/layout/Particles/ParticlesJS.jsx
+++ src/layout/Particles/ParticlesJS.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import './ParticlesJS.css'
 function ParticlesJS(props) {
-  particlesJS.load("particles-container", "particlesjs-config.json");
+  particlesJS.load("particles-js", "particlesjs-config.json");
   return (
-    <div id="particles-container">
+    <div id="particles-js">
       {props.children}
     </div>
   );
