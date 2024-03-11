3e52838171ff94487c0970c4f014fec1781fdaa1
Gui webber <guiz1n.webber@gmail.com>
2024-03-11 10:16:11 -0300

    finish section projects

--- src/layout/Projects/Projects.jsx
+++ src/layout/Projects/Projects.jsx
@@ -4,27 +4,28 @@ import "../darkMode.css";
 import { useDarkMode } from "../DarkModeContext";
 
 function Projects() {
+  const { isDarkMode } = useDarkMode();
   const [positions, setPositions] = useState([
     {
       title: "Valorant Guide",
       description:
         "Projeto utilizando a api de valorant, e utilizando as tecnologias HTML, CSS e JavaScript",
-      image: "url_da_imagem_1",
-      projectLink: "",
+      image: "../../src/images/valorantguide.png",
+      projectLink: "https://valorant-guide.vercel.app/index.html",
     },
     {
       title: "Cartão interativo",
       description:
         "Projeto com intuito de criar uma página de cadastro de cartão de crédito, com interatividade, utilizando HTMl, CSS e JavaScript",
-      image: "url_da_imagem_2",
-      projectLink: "interactive-card-form-six.vercel.app/",
+      image: "../../src/images/cartao.png",
+      projectLink: "https://interactive-card-form-six.vercel.app/",
     },
     {
       title: "Página principal de notícias",
       description:
         "Projeto focado em uma landing page, focada em posicionamento de elementos, utilizando somente HTML e CSS ",
-      image: "url_da_imagem_3",
-      projectLink: "news-homepage-grid.vercel.app/",
+      image: "../../src/images/news-page.png",
+      projectLink: "https://news-homepage-grid.vercel.app/",
     },
   ]);
 
@@ -43,9 +44,13 @@ function Projects() {
   };
 
   return (
-    <div className="containerProjects">
+    <div
+      className={`containerProjects ${
+        isDarkMode ? "projects-white" : "projects-black"
+      }`}
+    >
       <button className="btnLeft" onClick={handleLeftClick}>
-      ⬅
+        ⬅
       </button>
       {positions.map((project, index) => (
         <div
@@ -56,18 +61,28 @@ function Projects() {
               : index === positions.length - 1
               ? "right"
               : "center"
-          }`}
+          } ${isDarkMode ? "box-black" : "box-white"}`}
         >
-          <img src={project.image} alt={project.title} />
+          <img
+            className="imageProject"
+            src={project.image}
+            alt={project.title}
+          />
           <h2>{project.title}</h2>
           <p>{project.description}</p>
-          <a className="projectLink" href={project.link}>
+          <a
+            target="_blank"
+            className={`projectLink ${
+              isDarkMode ? "link-black" : "link-white"
+            }`}
+            href={project.projectLink}
+          >
             Link do projeto
           </a>
         </div>
       ))}
       <button className="btnRight" onClick={handleRightClick}>
-      ⮕
+        ⮕
       </button>
     </div>
   );
