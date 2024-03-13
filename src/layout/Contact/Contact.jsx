a85535221d920ec134bc9e8690585c50e923b0aa
Gui webber <guiz1n.webber@gmail.com>
2024-03-13 13:43:17 -0300

    add email form

--- src/layout/Contact/Contact.jsx
+++ src/layout/Contact/Contact.jsx
@@ -1,20 +1,64 @@
-import React from "react";
-import styles from "./Contact.module.css";
-import { useDarkMode } from "../DarkModeContext";
-import { FaLinkedin } from "react-icons/fa";
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 
-import "../darkMode.css";
 function Contact() {
-  
-  const { isDarkMode } = useDarkMode();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.init('dnxHT1l7StqvPDwZm'); // Initialize EmailJS with your user ID
+
+    const serviceID = 'default_service';
+    const templateID = 'template_y5ktjt5';
+
+    const templateParams = {
+      name: name,
+      email: email,
+      message: message
+    };
+
+    emailjs.send(serviceID, templateID, templateParams)
+      .then((response) => {
+        console.log("Email enviado com sucesso", response.status, response.text);
+        alert("Sua mensagem foi enviada, obrigado!");
+        // Limpar os campos do formulário após o envio bem-sucedido
+        setName('');
+        setEmail('');
+        setMessage('');
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("Oops! Algo deu errado com sua mensagem, por favor atualize e mandar novamente");
+      });
+  };
+
   return (
-    <div
-      className={`${styles.mainContact} ${
-        isDarkMode ? "title-dark" : "title-light"
-      }`}
-    >
-      <h1>Contatos</h1>
-    </div>
+    <form className="form" id="form" onSubmit={sendEmail}>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Seu Nome"
+        required
+      />
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Seu Email"
+        required
+      />
+      <textarea
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Sua Mensagem"
+        required
+      />
+      <button type="submit" className="btn btn-primary" id="btn-submit">Enviar</button>
+    </form>
   );
 }
 
